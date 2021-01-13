18b79a582503db4d4f5143c82f1a1ca359e88ddd
eric <48635802+ericziethen@users.noreply.github.com>
2021-01-13 19:38:49 +1100

    Initial project files

---
+++ tests/ez_sofifa_scraper/test_project_logger.py
@@ -0,0 +1,13 @@
+import os
+
+from ez_sofifa_scraper import project_logger
+
+
+def test_logging(tmpdir):
+    if not os.path.exists(tmpdir):
+        os.makedirs(tmpdir)
+
+    log_file = os.path.join(tmpdir, 'test_log.txt')
+
+    project_logger.setup_logger(log_file)
+    project_logger.test_logging()
