e60c567939195e586c0de89fd451ea1c97fdba4a
eric <48635802+ericziethen@users.noreply.github.com>
2021-01-14 21:21:26 +1100

    misc

---
+++ tests/ez_sofifa_scraper/test_scrape_data_player.py
@@ -0,0 +1,17 @@
+
+
+
+def test_read_player_row():
+    assert False
+
+
+def test_convert_player_data():
+    assert False
+
+
+def test_read_html_file():
+    assert False
+
+
+def test_read_multiple_html_files():
+    assert False
